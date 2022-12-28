df26e18339bf8d8be5df3a481a4bb1fd7acf9975
ANDREAS ZELIOS <antreaszel@gmail.com>
2022-12-28 22:02:21 +0200

    added a class of students and displayed each studets as a cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import "./App.css";
 import About from "./Components/About";
 import Info from "./Components/Info";
 import Footer from "./Components/Footer";
+import StudentCard from "./Components/StudentCard";
+import { StudentClass } from "./ClassOfStudents.js";
 
 export default function App() {
+  let Students = StudentClass.map((current_student) => {
+    return (
+      <StudentCard
+        StudentName={current_student.Student_Name}
+        StudentAM={current_student.Student_AM}
+        Class={current_student.Class}
+      />
+    );
+  });
+
+  console.log(Students);
+
   return (
     <div className="App">
       <Info />
+      <h2>Students</h2>
+      {Students}
       <About />
       <Footer />
     </div>
